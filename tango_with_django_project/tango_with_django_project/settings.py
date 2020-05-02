b15214f471a44cec45d675cfd77398c630e81ae2
matthewmacf <57261623+matthewmacf@users.noreply.github.com>
2020-05-02 16:48:00 +0100

    chapter 9 User Profile

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -102,7 +102,16 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': { 'min_length': 6, }
+    }
 ]
+
+PASSWORD_HASHERS = [ 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+                     'django.contrib.auth.hashers.BCryptPasswordHasher',
+                     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+                     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+                     ]
 
 
 # Internationalization
