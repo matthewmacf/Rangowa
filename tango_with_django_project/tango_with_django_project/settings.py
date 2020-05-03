267dfe6817a88d070571c3cc30e83bcbf53758bf
matthewmacf <57261623+matthewmacf@users.noreply.github.com>
2020-05-03 19:10:00 +0100

    fixing photos on python anywhhere

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
